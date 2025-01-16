61f149bfbd97dfbbfdf6eaaa793157910d272219
Facundo Muñoz <127797070+facuDefalt@users.noreply.github.com>
2025-01-16 15:24:38 -0300

    Add files via upload
    subida del proyecto editado

---
+++ RegistroInicioUsuario/src/registroiniciousuario/RegistroInicioUsuario.java
@@ -0,0 +1,14 @@
+package registroiniciousuario;
+
+import igu.Loguin;
+
+
+public class RegistroInicioUsuario {
+
+    public static void main(String[] args) {
+        Loguin logueo=new Loguin();
+        logueo.setVisible(true);
+        logueo.setLocationRelativeTo(null);
+    }
+    
+}
