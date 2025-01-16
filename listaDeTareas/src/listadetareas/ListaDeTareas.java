78b148d130759899678ab3e258c3aac996b1b690
Facundo Muñoz <127797070+facuDefalt@users.noreply.github.com>
2025-01-16 15:23:40 -0300

    Add files via upload

---
+++ listaDeTareas/src/listadetareas/ListaDeTareas.java
@@ -0,0 +1,14 @@
+package listadetareas;
+
+import IGU.ListaTareas;
+
+
+public class ListaDeTareas {
+
+    
+    public static void main(String[] args) {
+       ListaTareas lista=new ListaTareas();
+       lista.setVisible(true);
+    }
+    
+}
